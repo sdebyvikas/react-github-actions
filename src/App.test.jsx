ca7f045c9072636ddb82787d33aa2c7fddac27c8
Vikas Kumar <v535784@gmail.com>
2026-09-09 22:53:31 +0530

    run test cases

---
+++ src/App.test.jsx
@@ -0,0 +1,31 @@
+
+import { render, screen, cleanup } from "@testing-library/react";
+import { afterEach, expect, test } from "vitest";
+import "@testing-library/jest-dom/vitest";
+import App from "./App";
+
+afterEach(() => {
+  cleanup();
+});
+
+test("renders Vikas heading", () => {
+  render(<App />);
+
+  const heading = screen.getByRole("heading", {
+    level: 1,
+    name: /vikas kumar new code/i,
+  });
+
+  expect(heading).toBeInTheDocument();
+});
+
+test("renders branch heading", () => {
+  render(<App />);
+
+  const heading = screen.getByRole("heading", {
+    level: 2,
+    name: /react github action branch 2/i,
+  });
+
+  expect(heading).toBeInTheDocument();
+});
